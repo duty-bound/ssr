52effb91206ad718954c71f2c7c69f0b757fdbd4
Stephen <stephen.saliba@gmail.com>
2020-10-18 22:22:06 +0200

    Completes Chapter 2

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => {
+    return (
+        <>
+            <div>Hello world!</div>
+            <img src="/static/kitten.jpg" alt="kitten" />
+        </>
+    )
+}
