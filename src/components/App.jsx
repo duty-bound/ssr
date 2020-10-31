2dc02a4441120fe6b8b87c1bca719199776de78d
Stephen <stephen.saliba@gmail.com>
2020-10-31 21:14:22 +0100

    Completes Chapter 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 
 export default () => {
+    const env = __isClientSide__ ? 'client' : 'server'
     return (
         <>
-            <div>Hello world!</div>
+            <div>Hello world from {env}!</div>
             <img src="/static/kitten.jpg" alt="kitten" />
         </>
     )
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.scss'
 
+setTimeout(() => {
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 )
+}, 1000)
